c7e50cdd0a8d6a9b6ca9308520345fe472844c50
Waris Haleem <43011742+iAmWaris97@users.noreply.github.com>
2022-09-20 23:39:39 +0430

    Delete all the boilerplate from CRA (text, images, styles).

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Math Magician Day-1
-        </p>
-        <a
-          className="project-link"
-          href="https://github.com/iAmWaris97/math-magician"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This Project on GitHub
-        </a>
-      </header>
+      <p className="Waris">Test</p>
     </div>
   );
 }
